1c97db7903bc2c76e995d789bc1a351afd755f60
Damien Maguire <listerlt1@gmail.com>
2026-09-23 15:47:26 +0100

    PA15 brake INPUT_PU for 5 V CHST after divider

--- include/digio_prj.h
+++ include/digio_prj.h
@@ -26,7 +26,7 @@
 #define DIG_IO_LIST                                                            \
   DIG_IO_ENTRY(HV_req, GPIOD, GPIO5, PinMode::INPUT_FLT)                       \
   DIG_IO_ENTRY(start_in, GPIOD, GPIO7, PinMode::INPUT_FLT)                     \
-  DIG_IO_ENTRY(brake_in, GPIOA, GPIO15, PinMode::INPUT_FLT)                    \
+  DIG_IO_ENTRY(brake_in, GPIOA, GPIO15, PinMode::INPUT_PU)                     \
   DIG_IO_ENTRY(fwd_in, GPIOB, GPIO4, PinMode::INPUT_FLT)                       \
   DIG_IO_ENTRY(rev_in, GPIOB, GPIO3, PinMode::INPUT_FLT)                       \
   DIG_IO_ENTRY(dcsw_out, GPIOC, GPIO7, PinMode::OUTPUT)                        \
@@ -38,6 +38,7 @@
   DIG_IO_ENTRY(sw_mode1, GPIOD, GPIO8, PinMode::OUTPUT)                        \
   DIG_IO_ENTRY(lin_wake, GPIOD, GPIO10, PinMode::OUTPUT)                       \
   DIG_IO_ENTRY(lin_nslp, GPIOD, GPIO11, PinMode::OUTPUT)                       \
+  DIG_IO_ENTRY(lin_out, GPIOD, GPIO11, PinMode::OUTPUT)                        \
   DIG_IO_ENTRY(prec_out, GPIOC, GPIO6, PinMode::OUTPUT)                        \
   DIG_IO_ENTRY(inv_out, GPIOA, GPIO8, PinMode::OUTPUT)                         \
   DIG_IO_ENTRY(SL1_out, GPIOC, GPIO9, PinMode::OUTPUT)                         \
@@ -59,8 +60,5 @@
   DIG_IO_ENTRY(t15_digi, GPIOD, GPIO6, PinMode::INPUT_FLT)                     \
   DIG_IO_ENTRY(gp_12Vin, GPIOD, GPIO4, PinMode::INPUT_FLT)                     \
   DIG_IO_ENTRY(dummypin, GPIOE, GPIO7, PinMode::INPUT_PD)
-
-// dummypin is used by IOMatrix class for unused functions. Must be set to a pin
-// that has no effect
 
 #endif // PinMode_PRJ_H_INCLUDED
