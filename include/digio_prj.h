1247bf2ab059c067f8b5e69a29ca9d6255fbc4fa
Damien Maguire <listerlt1@gmail.com>
2026-09-24 14:41:57 +0100

    v15: PSU_EN PE0 for sleep latch

--- include/digio_prj.h
+++ include/digio_prj.h
@@ -56,11 +56,9 @@
   DIG_IO_ENTRY(PWM1, GPIOA, GPIO6, PinMode::OUTPUT)                            \
   DIG_IO_ENTRY(CANEN,     GPIOD, GPIO0,  PinMode::OUTPUT)                      \
   DIG_IO_ENTRY(CANSBY,    GPIOD, GPIO1,  PinMode::OUTPUT)                      \
+  DIG_IO_ENTRY(PSU_EN,    GPIOE, GPIO0,  PinMode::OUTPUT)                      \
   DIG_IO_ENTRY(t15_digi, GPIOD, GPIO6, PinMode::INPUT_FLT)                     \
   DIG_IO_ENTRY(gp_12Vin, GPIOD, GPIO4, PinMode::INPUT_FLT)                     \
   DIG_IO_ENTRY(dummypin, GPIOE, GPIO7, PinMode::INPUT_PD)
-
-// dummypin is used by IOMatrix class for unused functions. Must be set to a pin
-// that has no effect
 
 #endif // PinMode_PRJ_H_INCLUDED
